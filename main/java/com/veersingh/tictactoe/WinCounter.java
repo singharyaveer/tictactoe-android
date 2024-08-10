688f74b2ba4fd89171e28d50af5989f3d6614ea1
aryaveer_singh <115604220+singharyaveer@users.noreply.github.com>
2024-08-10 10:32:42 +0530

    created WinCounter class to help in counting the current wins and draws of player going on in one game

---
+++ main/java/com/veersingh/tictactoe/WinCounter.java
@@ -0,0 +1,13 @@
+package com.veersingh.tictactoe;
+
+public class WinCounter {
+    public int xWins;
+    public int oWins;
+    public int draws;
+
+    public WinCounter(int xWins, int oWins, int draws){
+        this.xWins = xWins;
+        this.oWins = oWins;
+        this.draws = draws;
+    }
+}
